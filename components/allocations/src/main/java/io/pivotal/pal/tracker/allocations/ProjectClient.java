451e0e1c3fe3d0b530c11ed1a606fbb20200e124
Danny Sabana <dsabana@ucdavis.edu>
2019-08-29 15:05:24 -0600

    Setting up circuit breaker.

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,31 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private Map<Long, ProjectInfo> cacheProjectMap;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
+        this.cacheProjectMap = new ConcurrentHashMap<>();
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        this.cacheProjectMap.put(projectId, projectInfo);
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return this.cacheProjectMap.get(projectId);
     }
 }
